4a9cee5a4e3046a1fc95a9d1e6551ec8e5186060
nugroho tanjo <rizkyarchery@gmail.com>
2020-01-10 22:06:11 +0700

    first commit

---
+++ src/main/java/com/bandingin/rikky/RikkyApplication.java
@@ -0,0 +1,13 @@
+package com.bandingin.rikky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RikkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RikkyApplication.class, args);
+	}
+
+}
